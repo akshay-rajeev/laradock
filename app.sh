f07f0f6a733250475746cb72941af565c4fdfe00
Akshay <akshay.rajeev@agent-network.com>
2022-10-26 15:00:01 +0900

    adding docker optimizations

--- app.sh
+++ app.sh
@@ -2,35 +2,35 @@
 
 case $1 in
     init)
-        docker-compose build
-        docker-compose up -d --remove-orphans
-        docker-compose exec php composer install
-        docker-compose exec php php artisan migrate:fresh --seed
-        docker-compose exec node npm install
-        docker-compose exec node npm run dev
+        docker compose build
+        docker compose up -d --remove-orphans
+        docker compose exec php composer install
+        docker compose exec php php artisan migrate:fresh --seed
+        docker compose exec node npm install
+        docker compose exec node npm run build
         ;;
     start)
-        docker-compose up -d --remove-orphans
+        docker compose up -d --remove-orphans
         ;;
     artisan)
-        docker-compose exec php php $*
+        docker compose exec php php $*
         ;;
     migrate)
-        docker-compose exec php php artisan migrate:fresh --seed
+        docker compose exec php php artisan migrate:fresh --seed
         ;;
     composer)
-        docker-compose exec php $*
+        docker compose exec php $*
         ;;
     watch)
-        docker-compose exec node npm run dev
+        docker compose exec node npm run dev
         ;;
     build)
-        docker-compose exec node npm run build
+        docker compose exec node npm run build
         ;;
     npm)
-        docker-compose exec node $*
+        docker compose exec node $*
         ;;
     login)
-        docker-compose exec -it $2 sh
+        docker compose exec -it $2 sh
         ;;
 esac
--- setup-ec2.sh
+++ setup-ec2.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 sudo yum update -y
-sudo amazon-linux-extras install docker
+sudo amazon-linux-extras install docker -y
 sudo service docker start
 sudo systemctl enable docker
 sudo usermod -a -G docker ec2-user
+newgrp docker
+sudo yum install docker-compose-plugin
