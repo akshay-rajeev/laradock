de9ad7dbe2b97970070f38790360aa0d0c2dfecc
Akshay <akshay.rajeev@agent-network.com>
2022-11-17 18:39:30 +0900

    adding github action

--- app.sh
+++ app.sh
@@ -3,40 +3,40 @@
 case $1 in
     init)
         docker-compose --env-file src/.env up -d --remove-orphans --build
-        docker-compose --env-file src/.env exec php chown -R www-data storage
-        docker-compose --env-file src/.env exec php composer install
-        docker-compose --env-file src/.env exec php php artisan migrate:fresh --seed
-        docker-compose --env-file src/.env exec php php artisan key:generate
-        docker-compose --env-file src/.env exec node npm install
-        docker-compose --env-file src/.env exec node npm run build
+        docker-compose --env-file src/.env exec -T php chown -R www-data storage
+        docker-compose --env-file src/.env exec -T php composer install
+        docker-compose --env-file src/.env exec -T php php artisan migrate:fresh --seed
+        docker-compose --env-file src/.env exec -T php php artisan key:generate
+        docker-compose --env-file src/.env exec -T node npm install
+        docker-compose --env-file src/.env exec -T node npm run build
         ;;
     start)
         docker-compose --env-file src/.env up -d --remove-orphans
         ;;
     composer)
-        docker-compose --env-file src/.env exec php $*
+        docker-compose --env-file src/.env exec -T php $*
         ;;
     artisan)
-        docker-compose --env-file src/.env exec php php $*
+        docker-compose --env-file src/.env exec -T php php $*
         ;;
     migrate)
-        docker-compose --env-file src/.env exec php php artisan migrate:fresh --seed
+        docker-compose --env-file src/.env exec -T php php artisan migrate:fresh --seed
         ;;
     npm)
-        docker-compose --env-file src/.env exec node $*
+        docker-compose --env-file src/.env exec -T node $*
         ;;
     watch)
-        docker-compose --env-file src/.env exec node npm run dev
+        docker-compose --env-file src/.env exec -T node npm run dev
         ;;
     build)
-        docker-compose --env-file src/.env exec node npm run build
+        docker-compose --env-file src/.env exec -T node npm run build
         ;;
     validate)
-        docker-compose --env-file src/.env exec php ./vendor/bin/phpstan analyse --memory-limit=2G
-        docker-compose --env-file src/.env exec php ./vendor/bin/phpcbf ./app
-        docker-compose --env-file src/.env exec php php artisan test
+        docker-compose --env-file src/.env exec -T php ./vendor/bin/phpstan analyse --memory-limit=2G
+        docker-compose --env-file src/.env exec -T php ./vendor/bin/phpcbf ./app
+        docker-compose --env-file src/.env exec -T php php artisan test
         ;;
     login)
-        docker-compose --env-file src/.env exec -it $2 sh
+        docker-compose --env-file src/.env exec -T -it $2 sh
         ;;
 esac
--- setup-ec2.sh
+++ setup-ec2.sh
@@ -3,10 +3,11 @@
 sudo yum update -y
 sudo amazon-linux-extras install docker -y
 sudo usermod -a -G docker ec2-user
-newgrp docker << END
-END
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose;
 sudo chmod +x /usr/local/bin/docker-compose;
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 sudo systemctl enable docker
-sudo service docker start
+newgrp docker << END
+    sudo service docker start
+    ./app.sh init
+END
